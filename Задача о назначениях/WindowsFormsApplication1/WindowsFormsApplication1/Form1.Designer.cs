5f1cf5afaedc89d58a58fa3c4faf6747dc18e783
Jicei <31828588+Jicei@users.noreply.github.com>
2018-11-05 19:37:45 +0200

    Add files via upload

---
+++ Задача о назначениях/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(30, 79);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(511, 292);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 392);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 64);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Запуск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(409, 401);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Кол-во работников:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Кол-во работ:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(223, 392);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 64);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Задать розмерность";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(547, 79);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(142, 292);
+            this.listBox1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(549, -4);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 80);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Ячейки с минимальной затратой:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(199, 9);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 29);
+            this.numericUpDown1.TabIndex = 11;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(146, 42);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 29);
+            this.numericUpDown2.TabIndex = 12;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 468);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
---
+++ Задача о назначениях/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool start=false;//Переменная для проверки заданости размерности
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n, m, i, j, l, buf, cost, cur;
+            int j0;
+            //Потенциал строк и потенциал столбцов
+            int []u;
+            int[] v;
+            int[] p;//Хранение паросочетания каждой выбраной строки
+            int []way;//Номер предшествующего столбца
+            int []minv;//Вспомогательный минимум для каждого j столбца
+            int[,] a;//Масив искомых данных
+            bool[]used;
+            n = (int)(numericUpDown1.Value);
+            m = (int)(numericUpDown2.Value);
+            if (start == false)
+            {
+                MessageBox.Show("Не задана размерность.");
+                return;
+            }
+            if (n > m)//Зампена мест строка/столбец
+            {
+                buf = n;
+                n=m;
+                m=buf;
+            }
+            listBox1.Items.Clear();
+            //Задание размерности
+            a = new int[n+1, m+1];
+            u=new int[n+1];
+            v=new int[m+1];
+            p=new int[m+1];
+            way=new int[m+1];
+            used=new bool[m+1];
+            for (i = 1; i <= n; i++)
+            {
+                for (j = 1; j <= m; j++)
+                {
+                    try//Проверка на коректность ввода
+                    {
+                        a[i, j] = Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Неправильно введенные данные");
+                        return;
+                    }
+                }
+            }
+
+            for (i = 1; i <= n; ++i)
+            {
+                p[0] = i;
+                j0 = 0;
+                minv = new int[m + 1];
+                for (l = 1; l <= m; l++)
+                {
+                    minv[l] = 9999;
+                    used[l] = false;
+                }
+                do
+                {
+                    used[j0] = true;//Помечаем посещенный столбец и строку
+                    int i0 = p[j0], delta = 9999, j1 = 0;
+                    for (j = 1; j <= m; ++j)
+                    {
+                        if (!used[j])//Проход по элементам
+                        {
+                            cur = a[i0, j] - u[i0] - v[j];//Расчет вспомогательного минимума
+                            if (cur < minv[j]) 
+                            {
+                                minv[j] = cur;//запись минимума
+                                way[j] = j0;//Запись строки
+                            }
+                            if (minv[j] < delta)//Поиск минимума и запоминание строки
+                            {
+                                delta = minv[j];
+                                j1 = j;
+                            }
+                        }
+                    }
+                    for (j = 0; j <= m; ++j)//Пересчет потенциала
+                    {
+                        if (used[j])
+                        {
+                            u[p[j]] += delta;
+                            v[j] -= delta;
+                        }
+                        else
+                            minv[j] -= delta;
+                    }
+                    j0 = j1;//Строка минимума
+                } while (p[j0] != 0);//Нашли свободный столбец-выход из цикла
+                do//Нахождение увеличивающейся цепочки
+                {
+                    int j1 = way[j0];
+                    p[j0] = p[j1];
+                    j0 = j1;
+                } while (j0 != 0);
+            }
+            //Вывод нужных индексов
+            for (j=1; j<=m; ++j)
+            {
+                if (p[j] != 0)
+                {
+                    listBox1.Items.Add("Ячейки:"+p[j] + "-" + j);
+                }
+            }
+            //Стоимомть папросочетания. 
+            cost = -v[0];//0 элемент общий для мнимой строки и минимого столбца
+            label1.Text = "Минимальные затраты:"+Convert.ToString(cost);
+         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Задание розмерности
+            int n, m,buf;
+            n = (int)(numericUpDown1.Value);
+            m = (int)(numericUpDown2.Value);
+            start = true;
+            dataGridView1.Rows.Clear();
+            if (n > m)//Замена мест столбец/строка
+            {
+                buf = n;
+                n = m;
+                m = buf;
+                dataGridView1.RowCount = n + 1;
+                dataGridView1.ColumnCount = m + 1;
+                dataGridView1.Rows[0].Cells[0].Value = "№";
+                dataGridView1.Columns[0].Width = 100;
+                for (int i = 0; i < n; i++)
+                {
+                    dataGridView1.Rows[i + 1].Cells[0].Value = ("Работа:" + Convert.ToString(i+1));
+                }
+                for (int j = 0; j < m; j++)
+                {
+                    dataGridView1.Rows[0].Cells[j + 1].Value = ("Работник:"+Convert.ToString(j+1));
+                    dataGridView1.Columns[j + 1].Width = 130;
+                }
+            }
+            else
+            {
+                dataGridView1.RowCount = n + 1;
+                dataGridView1.ColumnCount = m + 1;
+                dataGridView1.Rows[0].Cells[0].Value = "№";
+                dataGridView1.Columns[0].Width = 130;
+                for (int i = 0; i < n; i++)
+                {
+                    dataGridView1.Rows[i + 1].Cells[0].Value = ("Работник:"+Convert.ToString(i+1));
+                }
+                for (int j = 0; j < m; j++)
+                {
+                    dataGridView1.Rows[0].Cells[j + 1].Value = ("Работа:" + Convert.ToString(j+1));
+                    dataGridView1.Columns[j + 1].Width = 100;
+                }
+            }
+        }
+     }
+  }
+
